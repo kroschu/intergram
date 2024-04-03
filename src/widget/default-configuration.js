b564a8f5ee85ae79913a507cdc14cb39fba7da1a
kroschu <trebatak@hotmail.com>
2024-04-03 12:46:51 +0300

    update

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -1,19 +1,19 @@
 
 export const defaultConfiguration = {
-    titleClosed: 'Click to chat!',
-    titleOpen: 'Let\'s chat!',
-    closedStyle: 'chat', // button or chat
+    titleClosed: 'Cliquer pour discuter!',
+    titleOpen: 'Discutons!',
+    closedStyle: 'Discussion', // button or chat
     closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
     introMessage: 'Hello! How can we help you?',
-    autoResponse: 'Looking for the first available admin (It might take a minute)',
-    autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
-    'contact you, and we will get back to you as soon as we can.',
-    placeholderText: 'Send a message...',
+    autoResponse: 'Un moment, le message est envoyé.',
+    autoNoResponse: 'Je ne pense pas pouvoir répondre maintenant ' +
+    'Laissez-moi vos coordonnées et je vous répondrai dès que possible.',
+    placeholderText : 'Envoyer un message...',
     displayMessageTime: true,
     mainColor: '#1f8ceb',
     alwaysUseFloatingButton: false,
     desktopHeight: 450,
     desktopWidth: 370,
-    visitorPronoun: 'You'
+    visitorPronoun: 'Vous'
 };
